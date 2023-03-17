4174c5b3e37b7348b45c9116ee2fa51eec70db99
Tran Viet Long <longsupper212@gmail.com>
2023-03-17 15:21:39 +0700

    Update route

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ const morgan = require('morgan')
 const path = require('path'); 
 const { engine } = require('express-handlebars');
 const app = express()
-
+const route = require('./routes/index.js')
 
 //view engine setup
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
-
+route(app);
 //Static Folder
 // app.use('/public', express.static(path.join(__dirname, 'public')))
 
@@ -22,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+
 
 
 app.listen(3000)
