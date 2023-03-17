8a16509a6ba32e12ebd5bd4593deba170a077f16
Tran Viet Long <longsupper212@gmail.com>
2023-03-17 14:16:23 +0700

    Update route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 const path = require('path'); 
 const { engine } = require('express-handlebars');
 const app = express()
-
+const route = require('./routes/index.js')
+ 
+route(app);
 
 //view engine setup
 app.engine('.hbs', engine({extname: '.hbs'}));
@@ -12,16 +14,17 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 
 //Static Folder
-// app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use(express.urlencoded({
+  extended: "true"
+}));
+app.use(express.json());
 
 
 app.listen(3000)
---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const newscontroller = require("../app/controllers/newsController")
+router.use('/:slug', newscontroller.show)
+router.use('/', newscontroller.index)
+
+
+
+
+module.exports = router;
